2e45c98eb46b404fbef25054c6b7ac71f1f41d71
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-17 15:54:47 +0300

    client add app types

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,52 @@
 /// <reference types="vite/client" />
+
+// ErrorBoundary
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+}
+
+interface Answer {
+  id: number;
+  value: string;
+}
+
+interface Answers {
+  [index: number]: string;
+}
+
+interface Word {
+  id: number;
+  pos: string;
+  word: string;
+}
+
+interface RankContextType {
+  words: Word[];
+  answers: Answers;
+  reset: () => void;
+  setWords: (word: Word[]) => void;
+  addAnswer: (answer: Answer) => void;
+}
+
+interface WrapperProps {
+  title: string;
+  children: React.ReactNode;
+  isLoading: boolean;
+  error: Error | string;
+}
+
+interface SingleAnswerProps {
+  onChange: (answer: string) => void;
+  title: string;
+  answer: string;
+}
+
+interface AnswersProps {
+  setAnswer: (answer: string) => void;
+  answer: string;
+}
